88e4121de8d928b7f416766c05dd9b1f45e5a711
shyam <sshyamvikram@gmail.com>
2021-07-10 16:04:59 +0530

    -m

--- mysite/core/models.py
+++ mysite/core/models.py
@@ -5,12 +5,11 @@ class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     pdf = models.FileField(upload_to='books/pdfs/')
-    cover = models.ImageField(upload_to='books/covers/', null=True, blank=True)
+
 
     def __str__(self):
         return self.title
 
     def delete(self, *args, **kwargs):
         self.pdf.delete()
-        self.cover.delete()
         super().delete(*args, **kwargs)
